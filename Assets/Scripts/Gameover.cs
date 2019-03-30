266e32d65fd1b653b90eecf2cffd159dc6f3c1ab
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-03-30 18:24:26 -0400

    Add files via upload

---
+++ Assets/Scripts/Gameover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+       public void Reset(string SampleScene)
+        {
+        StartCoroutine(LoadYourAsyncScene());
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Should load scene");
+        }
+    
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
