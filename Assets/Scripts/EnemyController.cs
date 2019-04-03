03e0e3f73ef67976e346a70ee420bbb8766e3007
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-04-03 14:49:01 -0400

    Add files via upload

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,7 +7,8 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 10f;
-
+    public Transform[] points;
+    public int destPoint = 0;
     public Transform target;
     NavMeshAgent agent;
 
@@ -17,12 +18,16 @@ public class EnemyController : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player").transform;
         //target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        GotoNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
 
         if (distance <= lookRadius)
         {
@@ -40,6 +45,19 @@ public class EnemyController : MonoBehaviour
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 
     private void OnDrawGizmosSelected()
