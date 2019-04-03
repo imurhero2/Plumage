d84de70e429dd8a944516d71d9641254891072f0
imurhero2 <35977754+imurhero2@users.noreply.github.com>
2019-04-03 14:07:26 -0400

    Fixed PlayerManager Script

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
-    #region Singleton
 
-    public static PlayerManager instance;
+    public int startingHealth;
+    public static int health;
+    public static int featherCount;
+    public Text healthText;
+    public Text featherText;
 
-    private void Awake()
+    void Start()
     {
-        instance = this;
+        health = startingHealth;
+        featherCount = 0;
+        healthText.text = "Health: " + health;
+        featherText.text = "Feathers: " + featherCount;
     }
 
-    #endregion
+    private void ModifyHealth(int modifier)
+    {
+        health += modifier;
+        healthText.text = "Health: " + health;
+    }
 
-    public GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Projectile")
+        {
+            Destroy(other.gameObject);
+            if (health > 0)
+            {
+                ModifyHealth(-1);
+            }
+            if (health == 0)
+            {
+                // Trigger game over
+                Debug.Log("Game Over Scrub!");
+            }
+        }
+
+        if (other.tag == "Feather")
+        {
+            featherCount += 1;
+            Destroy(other.gameObject);
+            featherText.text = "Feathers: " + featherCount;
+        }
+    }
 }
