06bedf8487d8fa3ac3f3d79ae7cc947c6800f064
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-04-04 19:33:44 -0400

    Add files via upload

---
+++ Assets/Scripts/BaseProjectile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseProjectile : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    public abstract void FireProjectile(GameObject projectile, GameObject target);
+}
---
+++ Assets/Scripts/NormalProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalProjectile : BaseProjectile
+{
+    public float hangTime;
+
+    bool fired;
+
+    Vector3 direction;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (fired)
+        {
+            transform.position += direction * (speed * Time.deltaTime);
+        }
+
+        Tidy();
+    }
+
+    public override void FireProjectile(GameObject projectile, GameObject target)
+    {
+        if (projectile && target)
+        {
+            direction = (target.transform.position - projectile.transform.position).normalized;
+            fired = true;
+        }
+    }
+
+    void Tidy()
+    {
+        Destroy(this.gameObject, hangTime);
+    }
+}
