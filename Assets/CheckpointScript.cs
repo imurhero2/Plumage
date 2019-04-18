cb02a4ff5730dbd42e4f08553f80964c86ba7551
Jean-Paul <imurhero2@users.noreply.github.com>
2019-04-18 15:55:33 -0400

    Restored Commit
    Reverted back to commit f5a5516568

---
+++ Assets/CheckpointScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour {
+
+    private bool checkpointReached;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !checkpointReached)
+        {
+            checkpointReached = true;
+            Debug.Log("Checkpoint Reached!");
+        }
+    }
+
+}
