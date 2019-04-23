1cac1e3d16a6651a8ba725480b43909d5bf95e36
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-04-23 16:04:06 -0400

    Add files via upload

---
+++ Assets/Scripts/Enemies/FlyingAIGuide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAIGuide : MonoBehaviour
+{
+    public float speed;
+    public float width;
+    public float height;
+    public float length;
+
+    private float x, y, z;
+    private float counter;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        x = Mathf.Cos(counter) * width;
+        y = Mathf.Sin(counter) * height;
+        z = Mathf.Sin(counter) * length;
+
+        transform.position = startPos + new Vector3(x, y, z);
+    }
+}
