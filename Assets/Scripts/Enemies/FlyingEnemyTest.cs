9d688803375c790f901718964003e308df371776
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-04-24 16:01:23 -0400

    Add files via upload

--- Assets/Scripts/Enemies/FlyingEnemyTest.cs
+++ Assets/Scripts/Enemies/FlyingEnemyTest.cs
@@ -74,12 +74,13 @@ public class FlyingEnemyTest : MonoBehaviour
 
     IEnumerator lookAt()
     {
-        yield return new WaitForSecondsRealtime(delayAttack);
+        
         if (playerClose == true)
         {
             Vector3 direction = (target.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            yield return new WaitForSecondsRealtime(delayAttack);
         }
 
         // transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
